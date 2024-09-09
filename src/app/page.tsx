6a38fa14619444d510801ec50ac3653ba903a589
iamam305 <ajitesh314mishra@gmail.com>
2024-09-09 13:04:54 +0530

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 
 import { create_questions } from "@/actions/generate-questions";
 import SubmitButton from "@/components/SubmitButton";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
